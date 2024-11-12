834b48be413c4af8c1665e675b7668b8020b3871
FAZLUfasu <fasufazlu81@gmail.com>
2024-11-12 22:16:13 +0530

    after merge

--- fazlu/urls.py
+++ fazlu/urls.py
@@ -3,11 +3,11 @@ from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = ([
     path('grappelli/', include('grappelli.urls')),  # grappelli URLS
     path('admin', admin.site.urls),
     path('app/', include('fasu.urls')),  
+    path('', include('landpageapp.urls')), 
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 )
