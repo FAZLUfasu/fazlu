4d9861a0c3447a5360716067b6d3fce20ea0bc1f
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-10 20:04:04 +0530

    notification

--- fazlu/urls.py
+++ fazlu/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = ([
     path('', include('landpageapp.urls')),
@@ -10,6 +11,10 @@ urlpatterns = ([
     path('cadmin/', include('cadmin.urls',namespace='cadmin')),
     path('app/',include('fasu.urls')), 
     path('unixapp/',include('unixapp.urls')),
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+    path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 )
