5410acf6ed33b0d2d84b49f3ec6a9b978607cfe2
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-10 22:59:50 +0530

    notification

--- fasu/urls.py
+++ fasu/urls.py
@@ -1,3 +1,55 @@
+# from django.db import router
+# from fasu import views
+# from django.urls import path, include
+# from rest_framework import routers
+# from django.conf.urls.static import static
+# from django.conf import settings
+# from django.contrib.auth import views as auth_views
+
+
+# router = routers.DefaultRouter()
+# router.register(r'team_photos', views.TeamMemberViewSet)
+
+
+# urlpatterns =([
+#     path('get_notifications/',views.get_notifications,name='get_notifications'),
+#     path('notifications/', views.NotificationListView.as_view(), name='notification_list'),
+#     path('Whatsappchat/', views.WhatsappchatView.as_view(), name='Whatsappchat'),
+#     path('news/', views.NewsUpdateListCreateView.as_view(), name='news-list-create'),
+#     path('summary/', views.SummaryAPIView.as_view(), name='summary'),
+#     path('gallery/', views.gallery_data, name='gallery_data'),
+#     path('videos/',views.VideouploadView.as_view(), name='video-upload'),
+#     path('upvideos/',views.VideoListView.as_view(), name='videos'),
+#     path('images/',views. ImageUploadView.as_view(),name='image-upload'),
+#     path('up/', views.ImageListView.as_view(), name='images'),
+#     path('team_photos/', include(router.urls)),
+#     path('loginn/',views.LoginView.as_view()),
+#     path('investorprofilecreate/', views.create_investor_profile, name='investorprofilecreate'),
+#     path('investorprofile/', views.InvestorProfileAPIView.as_view(), name='investorprofile'),
+#     path('HomePageData/', views.HomePageDataAPIView.as_view(), name='HomePageData'), 
+#     path('myprojects/', views.MyProjectsAPIView.as_view(), name='myproject-list-create'),
+#     path('Projectpage/', views.ProjectpageListAPIView.as_view(), name='Projectpage'), 
+#     path('join-table/', views.join_table_list, name='join-table-list'),
+#     path('teammember/',views.TeamMemberListAPIView.as_view(), name='teammember'),
+#     path('contactinfo/', views.ContactInfoAPIView.as_view(), name='contact_info'),
+#     path('AboutUs/', views.AboutUsPageAPIView.as_view(), name='AboutUs'),
+#     path('investorprofile/username/<str:username>/', views.profile_view),
+#     path('myprojects/username/<str:username>/', views. my_projects_view),
+#     path('forgot-password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+#     path('register/', views.register, name='register'),
+#     path('get_video_notifications/', views.get_video, name='video_notifications'),
+#     path('videonotifications/', views.NotificationListView.as_view(), name='notification_list'),
+#     path('create-join-request/', views.create_join_request, name='create_join_request'),
+#     path('user-details/', views.UserDetailView.as_view(), name='user-details'),
+     
+   
+
+#     ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# )
+
+
+
+
 from django.db import router
 from fasu import views
 from django.urls import path, include
@@ -6,43 +58,43 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 
-
+# Initialize the router for the TeamMemberViewSet
 router = routers.DefaultRouter()
 router.register(r'team_photos', views.TeamMemberViewSet)
 
-
-urlpatterns =([
-    path('get_notifications/',views.get_notifications,name='get_notifications'),
+# Define the urlpatterns
+urlpatterns = [
+    path('get_notifications/', views.get_notifications, name='get_notifications'),
     path('notifications/', views.NotificationListView.as_view(), name='notification_list'),
     path('Whatsappchat/', views.WhatsappchatView.as_view(), name='Whatsappchat'),
     path('news/', views.NewsUpdateListCreateView.as_view(), name='news-list-create'),
     path('summary/', views.SummaryAPIView.as_view(), name='summary'),
     path('gallery/', views.gallery_data, name='gallery_data'),
-    path('videos/',views.VideouploadView.as_view(), name='video-upload'),
-    path('upvideos/',views.VideoListView.as_view(), name='videos'),
-    path('images/',views. ImageUploadView.as_view(),name='image-upload'),
+    path('videos/', views.VideouploadView.as_view(), name='video-upload'),
+    path('upvideos/', views.VideoListView.as_view(), name='videos'),
+    path('images/', views.ImageUploadView.as_view(), name='image-upload'),
     path('up/', views.ImageListView.as_view(), name='images'),
     path('team_photos/', include(router.urls)),
-    path('loginn/',views.LoginView.as_view()),
+    path('loginn/', views.LoginView.as_view()),
     path('investorprofilecreate/', views.create_investor_profile, name='investorprofilecreate'),
     path('investorprofile/', views.InvestorProfileAPIView.as_view(), name='investorprofile'),
-    path('HomePageData/', views.HomePageDataAPIView.as_view(), name='HomePageData'), 
+    path('HomePageData/', views.HomePageDataAPIView.as_view(), name='HomePageData'),
     path('myprojects/', views.MyProjectsAPIView.as_view(), name='myproject-list-create'),
-    path('Projectpage/', views.ProjectpageListAPIView.as_view(), name='Projectpage'), 
+    path('Projectpage/', views.ProjectpageListAPIView.as_view(), name='Projectpage'),
     path('join-table/', views.join_table_list, name='join-table-list'),
-    path('teammember/',views.TeamMemberListAPIView.as_view(), name='teammember'),
+    path('teammember/', views.TeamMemberListAPIView.as_view(), name='teammember'),
     path('contactinfo/', views.ContactInfoAPIView.as_view(), name='contact_info'),
     path('AboutUs/', views.AboutUsPageAPIView.as_view(), name='AboutUs'),
     path('investorprofile/username/<str:username>/', views.profile_view),
-    path('myprojects/username/<str:username>/', views. my_projects_view),
+    path('myprojects/username/<str:username>/', views.my_projects_view),
     path('forgot-password/', auth_views.PasswordResetView.as_view(), name='password_reset'),
     path('register/', views.register, name='register'),
     path('get_video_notifications/', views.get_video, name='video_notifications'),
     path('videonotifications/', views.NotificationListView.as_view(), name='notification_list'),
     path('create-join-request/', views.create_join_request, name='create_join_request'),
     path('user-details/', views.UserDetailView.as_view(), name='user-details'),
-     
-   
+]
 
-    ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-)
+# Append static and media URLs
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- fazlu/urls.py
+++ fazlu/urls.py
@@ -1,21 +1,47 @@
+# from django.contrib import admin
+# from django.urls import path, include
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib.auth import views as auth_views
+
+# urlpatterns = ([
+#     path('', include('landpageapp.urls')),
+#     path('grappelli/', include('grappelli.urls')),
+#     path('admin/', admin.site.urls), 
+#     path('cadmin/', include('cadmin.urls',namespace='cadmin')),
+#     path('app/',include('fasu.urls')), 
+#     path('unixapp/',include('unixapp.urls')),
+
+#     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
+#     path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+#     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+#     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+# ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
+# +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# )
+
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 
-urlpatterns = ([
+urlpatterns = [
     path('', include('landpageapp.urls')),
     path('grappelli/', include('grappelli.urls')),
-    path('admin/', admin.site.urls), 
-    path('cadmin/', include('cadmin.urls',namespace='cadmin')),
-    path('app/',include('fasu.urls')), 
-    path('unixapp/',include('unixapp.urls')),
+    path('admin/', admin.site.urls),
+    path('cadmin/', include('cadmin.urls', namespace='cadmin')),
+    path('app/', include('fasu.urls')),
+    path('unixapp/', include('unixapp.urls')),
+
+    # Password reset views
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
-]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
-+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-)
+]
 
+# Serve static and media files in development
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
