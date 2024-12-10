176649511ef608a638351381c7c5665eca7a623a
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-10 23:27:08 +0530

    notification

--- fazlu/settings.py
+++ fazlu/settings.py
@@ -134,14 +134,13 @@ DATABASES = {
 
 # settings.py
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'unixaquaticsapp@gmail.com' 
 EMAIL_HOST_PASSWORD = 'Kl10at10'
-SITE_URL = "https://unix-aquatics.com.com"
+SITE_URL = "https://unix-aquatics.com"
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
