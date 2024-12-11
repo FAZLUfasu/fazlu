a6f5ff4ae05d4b1eba262745fc7432607abe242d
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-11 21:40:34 +0530

    2.0

--- unixapp/static/unixapp/flutter_service_worker.js
+++ unixapp/static/unixapp/flutter_service_worker.js
@@ -27,7 +27,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "c74ead7f965381e9ae8b693289fe3c55
 "canvaskit/skwasm.worker.js": "89990e8c92bcb123999aa81f7e203b1c",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "a135d63fa2a71b44e357b605857ffa7f",
+"flutter_bootstrap.js": "8c7013cc340bf170dce3a991fce73ae4",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
