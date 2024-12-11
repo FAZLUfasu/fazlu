86f548150afdc6603ec43453c5fa99eafa0ebd90
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-11 11:01:52 +0530

    notification

--- fazlu/urls.py
+++ fazlu/urls.py
@@ -26,7 +26,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from fasu import views
 
 urlpatterns = ([
     path('', include('landpageapp.urls')),
@@ -37,11 +36,11 @@ urlpatterns = ([
     path('unixapp/', include('unixapp.urls')),
 
 
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('reset_password/', auth_views.PasswordResetView.as_view(), name='reset_password'),
     path('reset_password_done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
-    path('reset-password-complete/', views.password_reset_complete, name='password_reset_complete'),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]+ static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT))
 
