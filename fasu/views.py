5dace9b3b1c8b8c628b557027fd5e6ecaecc6b09
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-13 19:19:21 +0530

    test

--- fasu/views.py
+++ fasu/views.py
@@ -26,9 +26,19 @@ from .models import Images,video
 # from .models import MyProject
 from rest_framework.parsers import MultiPartParser, FormParser
 from django.contrib.auth.models import User
-
-
-
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode  
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.models import User
+from django.contrib.auth.tokens import default_token_generator
+from django.utils.http import urlsafe_base64_encode
+from django.utils.encoding import force_bytes
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.conf import settings
+from django.contrib.auth.forms import SetPasswordForm
 from rest_framework_simplejwt.tokens import AccessToken
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.authtoken.models import Token
@@ -469,67 +479,6 @@ def register(request):
 
 
 
-import json
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
-from django.utils.http import urlsafe_base64_encode
-from django.contrib.auth.tokens import default_token_generator
-from django.shortcuts import render
-from django.http import JsonResponse
-from django.conf import settings
-from django.contrib.auth.forms import SetPasswordForm
-from django.shortcuts import render, redirect
-from django.template.loader import render_to_string
-from django.utils.http import urlsafe_base64_decode
-from django.views.decorators.csrf import csrf_exempt
-
-# @csrf_exempt
-# def reset_password(request):
-#     if request.method == 'POST':
-#         try:
-#             data = json.loads(request.body)
-#             email = data.get('email')
-
-#             if not email:
-#                 return JsonResponse({'error': 'Email is required'}, status=400)
-
-#             user = User.objects.filter(email=email).first()
-
-#             if not user:
-#                 return JsonResponse({'error': 'User with this email does not exist'}, status=404)
-
-#             # Generate the reset token and user ID
-#             uid = urlsafe_base64_encode(str(user.pk).encode())
-#             token = default_token_generator.make_token(user)
-
-#             # Generate reset URL (e.g., frontend URL)
-#             reset_url = f"{settings.FRONTEND_URL}/reset-password/{uid}/{token}/"
-
-#             # Build the email content
-#             subject = "Reset Your Password"
-#             message = render_to_string('password_reset_email.html', {'reset_url': reset_url})
-
-#             # Send email
-#             send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [user.email])
-
-#             return JsonResponse({'message': 'Password reset email sent!'}, status=200)
-
-#         except Exception as e:
-#             return JsonResponse({'error': str(e)}, status=500)
-
-#     return JsonResponse({'error': 'Invalid request method'}, status=405)
-
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth.models import User
-from django.contrib.auth.tokens import default_token_generator
-from django.utils.http import urlsafe_base64_encode
-from django.utils.encoding import force_bytes
-from django.core.mail import send_mail
-from django.template.loader import render_to_string
-from django.conf import settings
 
 
 class ResetPasswordView(APIView):
