112d14f10d1e3115de7425a4b5d25170050584db
FAZLUfasu <fasufazlu81@gmail.com>
2024-12-16 13:48:31 +0530

    test

--- fasu/models.py
+++ fasu/models.py
@@ -346,3 +346,15 @@ class Location(models.Model):
     class Meta:
         verbose_name = 'Location'
         verbose_name_plural = 'Locations'
+
+
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
